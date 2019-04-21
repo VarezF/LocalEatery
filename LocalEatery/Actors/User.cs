913d0bfbe93c8b8496e94b093f1992ccb91936c4
JarredEagley <jarred.eagley@gmail.com>
2019-04-21 16:16:27 -0700

    Adjustments to the fields in User.cs

--- LocalEatery/Actors/User.cs
+++ LocalEatery/Actors/User.cs
@@ -9,9 +9,9 @@ namespace Actors
     internal abstract class User
     {
         // Fields
-        private string name "";
+        private string name = "";
         private double wage = 0.0;
-        private Day[] current_schedule; // I might need some clarification on this.
+        private WorkDay[] current_schedule = new WorkDay[7];    // 7 workdays in a week. This might need changed.
 
         public void setName(string newname)
         {
