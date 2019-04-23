fb4f8e602c16fad8953218d9a07ef58a789af678
JarredEagley <jarred.eagley@gmail.com>
2019-04-22 20:26:43 -0700

    Added constructor for manager.

--- LocalEatery/Actors/Manager.cs
+++ LocalEatery/Actors/Manager.cs
@@ -9,9 +9,14 @@ namespace Actors
     class Manager : User
     {
 
-
-
-
+        /// <summary>
+        /// Constructor for a 'manager' user object.
+        /// </summary>
+        /// <param name="_name">Name of the manager.</param>
+        public Manager (String _name)
+        {
+            this.setName(_name);
+        }
 
     }
 }
