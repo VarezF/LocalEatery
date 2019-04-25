3fb0dc8b94b379c5c3a3ac4613dd1b85e10ecc29
Freya Varez <freyavarez@gmail.com>
2019-04-25 13:12:07 -0700

    References corrected, UI changes

--- LocalEatery/UI/Form1.Designer.cs
+++ LocalEatery/UI/Form1.Designer.cs
@@ -33,13 +33,20 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Dish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Table1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Table1_Status = new System.Windows.Forms.Label();
+            this.Table2_Status = new System.Windows.Forms.Label();
+            this.Table5_Status = new System.Windows.Forms.Label();
+            this.Table4_Status = new System.Windows.Forms.Label();
+            this.Table3_Status = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -55,8 +62,6 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -78,6 +83,17 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.Table3_Status);
+            this.tabPage3.Controls.Add(this.Table4_Status);
+            this.tabPage3.Controls.Add(this.Table5_Status);
+            this.tabPage3.Controls.Add(this.Table2_Status);
+            this.tabPage3.Controls.Add(this.Table1_Status);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Controls.Add(this.Table1);
+            this.tabPage3.Controls.Add(this.pictureBox2);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -86,29 +102,119 @@
             this.tabPage3.Text = "Waiter";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // pictureBox2
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(363, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(462, 386);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(361, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(462, 386);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
-            // dataGridView1
+            // Table1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Dish});
-            this.dataGridView1.Location = new System.Drawing.Point(380, 20);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(105, 100);
-            this.dataGridView1.TabIndex = 1;
+            this.Table1.AutoSize = true;
+            this.Table1.BackColor = System.Drawing.Color.Gainsboro;
+            this.Table1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Table1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Table1.Location = new System.Drawing.Point(715, 281);
+            this.Table1.Name = "Table1";
+            this.Table1.Size = new System.Drawing.Size(61, 16);
+            this.Table1.TabIndex = 2;
+            this.Table1.Text = "Table 4";
             // 
-            // Dish
+            // label1
             // 
-            this.Dish.HeaderText = "Dish";
-            this.Dish.Name = "Dish";
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(436, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Table 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(715, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Table 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gainsboro;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(578, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Table 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gainsboro;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(436, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Table 1";
+            // 
+            // Table1_Status
+            // 
+            this.Table1_Status.AutoSize = true;
+            this.Table1_Status.Location = new System.Drawing.Point(451, 53);
+            this.Table1_Status.Name = "Table1_Status";
+            this.Table1_Status.Size = new System.Drawing.Size(38, 13);
+            this.Table1_Status.TabIndex = 7;
+            this.Table1_Status.Text = "Ready";
+            // 
+            // Table2_Status
+            // 
+            this.Table2_Status.AutoSize = true;
+            this.Table2_Status.Location = new System.Drawing.Point(728, 53);
+            this.Table2_Status.Name = "Table2_Status";
+            this.Table2_Status.Size = new System.Drawing.Size(37, 13);
+            this.Table2_Status.TabIndex = 8;
+            this.Table2_Status.Text = "Status";
+            // 
+            // Table5_Status
+            // 
+            this.Table5_Status.AutoSize = true;
+            this.Table5_Status.Location = new System.Drawing.Point(728, 307);
+            this.Table5_Status.Name = "Table5_Status";
+            this.Table5_Status.Size = new System.Drawing.Size(38, 13);
+            this.Table5_Status.TabIndex = 9;
+            this.Table5_Status.Text = "Ready";
+            // 
+            // Table4_Status
+            // 
+            this.Table4_Status.AutoSize = true;
+            this.Table4_Status.Location = new System.Drawing.Point(451, 307);
+            this.Table4_Status.Name = "Table4_Status";
+            this.Table4_Status.Size = new System.Drawing.Size(38, 13);
+            this.Table4_Status.TabIndex = 10;
+            this.Table4_Status.Text = "Ready";
+            // 
+            // Table3_Status
+            // 
+            this.Table3_Status.AutoSize = true;
+            this.Table3_Status.Location = new System.Drawing.Point(589, 188);
+            this.Table3_Status.Name = "Table3_Status";
+            this.Table3_Status.Size = new System.Drawing.Size(38, 13);
+            this.Table3_Status.TabIndex = 11;
+            this.Table3_Status.Text = "Ready";
             // 
             // Form1
             // 
@@ -120,9 +226,9 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -133,9 +239,17 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Dish;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Table3_Status;
+        private System.Windows.Forms.Label Table4_Status;
+        private System.Windows.Forms.Label Table5_Status;
+        private System.Windows.Forms.Label Table2_Status;
+        private System.Windows.Forms.Label Table1_Status;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Table1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
